993ecb05cb63dcaad8c6eeff419b9666b528f3cb
Tim Huang <artines1@gmail.com>
2014-02-07 14:47:06 +0800

    feature: Power control - Change rate to Mbit from Kbit, and remove packet lost penalty, and fix the issue that packet error rate is incorrect

--- src/Rate_different_schemes/fcn_rate_calculate_with_PER.m
+++ src/Rate_different_schemes/fcn_rate_calculate_with_PER.m
@@ -7,6 +7,7 @@ rate_time_cost = packet_size./rate_byte_per_sec;
 
 real_PER_up = 0;
 real_PER_down = 0;
+packet_error_penalty=0;
 
 ave_rate=zeros(1,3);
 ave_time_cost=zeros(size(record_SINR,2),2); % column1: uplink time spend column2: down link time spend
@@ -21,7 +22,7 @@ for t=1:size(record_SINR,2)
         real_PER_up = 1;
     else
         for i=size(temp_idx, 1):-1:1
-            temp_per = 1 - (snrtable(temp_idx(i,1), 3) ^ packet_size);
+            temp_per = 1 - snrtable(temp_idx(i,1), 3);
             
             if temp_per <= target_PER
                 Rate_idx_up = i;                
@@ -37,7 +38,7 @@ for t=1:size(record_SINR,2)
         real_PER_down = 1;
     else
         for i=size(temp_idx, 1):-1:1
-            temp_per = 1 - (snrtable(temp_idx(i,1), 3) ^ packet_size);
+            temp_per = 1 - snrtable(temp_idx(i,1), 3);
             
             if temp_per <= target_PER
                 Rate_idx_down = i;
@@ -45,7 +46,7 @@ for t=1:size(record_SINR,2)
                 if size(real_rate_idx, 1) == 0
                     real_PER_down = 1;
                 else
-                    real_PER_down = 1 - (snrtable(real_rate_idx, 3) ^ packet_size);
+                    real_PER_down = 1 - snrtable(real_rate_idx, 3);
                 end
                 break;
             end
@@ -58,13 +59,13 @@ for t=1:size(record_SINR,2)
         if ave_success_transmit(t,1) == 1
             ave_time_cost(t,1)=rate_time_cost(Rate_idx_up);
         else
-            ave_time_cost(t,1)=rate_time_cost(Rate_idx_up) + (2 * 10 ^ -6); % add ack timeout
+            ave_time_cost(t,1)=rate_time_cost(Rate_idx_up) + packet_error_penalty;
         end
         
         if ave_success_transmit(t,2) == 1
             ave_time_cost(t,2)=rate_time_cost(Rate_idx_down);
         else
-            ave_time_cost(t,2)=rate_time_cost(Rate_idx_down) + (2 * 10 ^ -6);
+            ave_time_cost(t,2)=rate_time_cost(Rate_idx_down) + packet_error_penalty;
         end
         
     elseif record_SINR(1,t)==0&&record_SINR(2,t)~=0 % only downlink
@@ -72,14 +73,14 @@ for t=1:size(record_SINR,2)
         if ave_success_transmit(t,2) == 1
             ave_time_cost(t,2)=rate_time_cost(Rate_idx_down);
         else
-            ave_time_cost(t,2)=rate_time_cost(Rate_idx_down) + (2 * 10 ^ -6);
+            ave_time_cost(t,2)=rate_time_cost(Rate_idx_down) + packet_error_penalty;
         end
     elseif record_SINR(1,t)~=0&&record_SINR(2,t)==0% only uplink
         ave_success_transmit(t,1)=randsrc(1,1,[1 0; 1-real_PER_up real_PER_up]);
         if ave_success_transmit(t,1) == 1
             ave_time_cost(t,1)=rate_time_cost(Rate_idx_up);
         else
-            ave_time_cost(t,2)=rate_time_cost(Rate_idx_down) + (2 * 10 ^ -6);
+            ave_time_cost(t,2)=rate_time_cost(Rate_idx_down) + packet_error_penalty;
         end
     end
     
@@ -87,13 +88,13 @@ end
 if sum(packet_size.*ave_success_transmit(:,1)) == 0
     ave_rate(1,2) = 0;
 else
-    ave_rate(1,2)=sum(packet_size.*ave_success_transmit(:,1)) / sum(ave_time_cost(:,1)) / 1024 * 8;
+    ave_rate(1,2)=sum(packet_size.*ave_success_transmit(:,1)) / sum(ave_time_cost(:,1)) / (1024^2) * 8;
 end
 
 if sum(packet_size.*ave_success_transmit(:,2)) == 0
     ave_rate(1,3) = 0;
 else
-    ave_rate(1,3)=sum(packet_size.*ave_success_transmit(:,2)) / sum(ave_time_cost(:,2)) / 1024 * 8;
+    ave_rate(1,3)=sum(packet_size.*ave_success_transmit(:,2)) / sum(ave_time_cost(:,2)) / (1024^2) * 8;
 end
 
 ave_rate(1,1)=ave_rate(1,2) + ave_rate(1,3);
